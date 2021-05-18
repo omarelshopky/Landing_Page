8f637974670811928b8f7385155fd1517ec1d101
Omar Hesham <oommaarr.220022@gmail,com>
2021-05-18 22:01:57 +0200

    active section different from the other

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -20,6 +20,8 @@
 const fragment = document.createDocumentFragment();
 const navBar = document.getElementById('navbar__list');
 const sections = [...document.querySelectorAll('section')];
+let lastActiveSection = sections[0];
+const activeClass = 'active';
 
 
 /**
@@ -27,13 +29,66 @@ const sections = [...document.querySelectorAll('section')];
  * Start Helper Functions
  * 
 */
-// add section name to fragment to add leter to Navbar
-function addSectionToFragment(name){
+
+/**
+ * @description add section name to fragment to add leter to Navbar
+ * @param {string} name Section name will be shown in navBar
+ * @param {string} ID Section ID to create section navList ID
+ */
+function addSectionToFragment(name, ID){
     const listTag = document.createElement('li');
     listTag.textContent = name;
+    listTag.id = ID + 'nav';
     fragment.appendChild(listTag);
 }
 
+/**
+ * @description build Navbar according to sections in html
+ */
+function buildNavBar(){
+    for(const section of sections){
+        const secName = section.getAttribute('data-nav');
+        const secID = section.getAttribute('id');
+        addSectionToFragment(secName, secID);
+    }
+    navBar.appendChild(fragment);
+}
+
+/**
+ * @description check if the element in Viewport or not
+ * @param {*} element the element want to check its bounding
+ * @returns element bounding
+ */
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function changeSectionActivite(section){
+    if(lastActiveSection.id != section.id){
+        lastActiveSection.classList.toggle(activeClass);
+    }else{/* Do Nothing */}
+
+    if(!section.classList.contains(activeClass)){
+        section.classList.toggle(activeClass);
+    }else{/* Do Nothing */}
+    lastActiveSection = section;
+}
+
+function CheckActivite(){
+    for(const section of sections){
+        if(isInViewport(section)){
+            changeSectionActivite(section);
+        }else{
+            continue;
+        }
+    }
+}
 
 /**
  * End Helper Functions
@@ -42,14 +97,6 @@ function addSectionToFragment(name){
 */
 
 // build the nav
-function buildNavBar(SEC){
-    for(const section of SEC){
-        const secName = section.getAttribute('data-nav');
-        addSectionToFragment(secName);
-    }
-    navBar.appendChild(fragment);
-}
-
 buildNavBar(sections);
 
 // Add class 'active' to section when near top of viewport
@@ -69,5 +116,5 @@ buildNavBar(sections);
 // Scroll to section on link click
 
 // Set sections as active
-
+document.addEventListener('scroll', CheckActivite);
 
