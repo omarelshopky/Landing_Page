fdaea73406b2f32197d8a38de613e43f8d8db416
Omar Hesham <oommaarr.220022@gmail,com>
2021-05-18 20:55:35 +0200

    add secitons to navBar with js

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -18,13 +18,21 @@
  * 
 */
 const fragment = document.createDocumentFragment();
+const navBar = document.getElementById('navbar__list');
+const sections = [...document.querySelectorAll('section')];
+
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
+// add section name to fragment to add leter to Navbar
+function addSectionToFragment(name){
+    const listTag = document.createElement('li');
+    listTag.textContent = name;
+    fragment.appendChild(listTag);
+}
 
 
 /**
@@ -34,7 +42,15 @@ const fragment = document.createDocumentFragment();
 */
 
 // build the nav
+function buildNavBar(SEC){
+    for(const section of SEC){
+        const secName = section.getAttribute('data-nav');
+        addSectionToFragment(secName);
+    }
+    navBar.appendChild(fragment);
+}
 
+buildNavBar(sections);
 
 // Add class 'active' to section when near top of viewport
 
